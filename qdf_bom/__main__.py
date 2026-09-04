a15c2ebdc5650a61592a85c70600b7138de7d702
Florian Doersch <656249+Flow86@users.noreply.github.com>
2026-09-04 09:35:24 +0200

    Fix standalone executable

--- qdf_bom/__main__.py
+++ qdf_bom/__main__.py
@@ -1,6 +1,6 @@
 """python -m qdf_bom entry point."""
 
 import sys  # pragma: no cover
-from .tool import main  # pragma: no cover
+from qdf_bom.tool import main  # pragma: no cover
 
 sys.exit(main())  # pragma: no cover
